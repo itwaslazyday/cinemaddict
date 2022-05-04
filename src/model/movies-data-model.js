f6979eba94504e44256069bbfbd5ccd775516de0
Вячеслав <pudra@live.ru>
2022-05-04 17:28:03 +0500

    Рефакторинг кода

--- src/model/movies-data-model.js
+++ src/model/movies-data-model.js
@@ -2,8 +2,8 @@ import {generateMovieDescription} from '../fish/movie-description.js';
 import {generateMovieComments} from '../fish/movie-comments.js';
 
 export default class MoviesModel {
-  movies = generateMovieDescription();
-  getMovies = () => this.movies;
-  comments = generateMovieComments();
-  getComments = () => this.comments;
+  #movies = generateMovieDescription();
+  get movies() {return this.#movies;}
+  #comments = generateMovieComments();
+  get comments() {return this.#comments;}
 }
